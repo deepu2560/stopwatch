491e8e0c9f6f1da520bf7f933074a868ff627e69
unknown <deepanshugulia256@gmail.com>
2022-07-23 18:41:34 +0530

    started working on watch

---
+++ src/Components/stopwatch/stopwatch.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./stopwatch.css";
+
+export const Stopwatch = () => {
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+    </div>
+  );
+};
---
+++ src/Components/watch/watch.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./watch.css";
+
+export const Watch = () => {
+  return (
+    <div>
+      <h1>Watch</h1>
+    </div>
+  );
+};
