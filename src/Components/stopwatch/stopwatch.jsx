654a6d3e8e267a22da4507adf257337fa0a4cd55
unknown <deepanshugulia256@gmail.com>
2022-07-24 12:34:52 +0530

    stopwatch ui ready

--- src/Components/stopwatch/stopwatch.jsx
+++ src/Components/stopwatch/stopwatch.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./stopwatch.css";
 
 export const Stopwatch = () => {
+  const [stophour, setstophour] = useState("00");
+  const [stopmin, setstopmin] = useState("00");
+  const [stopsec, setstopsec] = useState("00");
+
+  const [stop, setstop] = useState(true);
+
+  const handlestart = () => {
+    setstop(() => false);
+  };
+
+  const handlestop = (action) => {
+    setstop(() => true);
+  };
   return (
-    <div>
+    <div id="stopwatch">
       <h1>Stopwatch</h1>
+      <div id="stopwatch-time-display">
+        <p>
+          <span>{stophour + " "} </span> : <span>{stopmin + " "}</span> :{" "}
+          <span>{stopsec + " "}</span>
+        </p>
+      </div>
+      {stop ? (
+        <button onClick={() => handlestart()}>START</button>
+      ) : (
+        <div>
+          <button onClick={() => handlestop(stop)}>RESTART</button>
+          <button onClick={() => handlestop(stop)}>STOP</button>
+        </div>
+      )}
     </div>
   );
 };
